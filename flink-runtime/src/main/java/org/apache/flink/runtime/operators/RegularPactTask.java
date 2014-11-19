6c4f476e041be8129d63b4f4a177ca5385b1a087
Ferenc Béres <ferdzso05@gmail.com>
2014-11-19 13:29:48 +0100

    headers and checkstyle errors corrected

--- flink-runtime/src/main/java/org/apache/flink/runtime/operators/RegularPactTask.java
+++ flink-runtime/src/main/java/org/apache/flink/runtime/operators/RegularPactTask.java
@@ -1023,12 +1023,12 @@ public class RegularPactTask<S extends Function, OT> extends AbstractInvokable i
 			MutableReader<Record> reader = (MutableReader<Record>) inputReader;
 			return new RecordReaderIterator(reader);
 		} else if(serializerFactory.getDataType().equals(MulticastMessage.class)) {
-            //NOTE: multicast refactor:
-            @SuppressWarnings("unchecked")
-            MutableReader<DeserializationDelegate<MulticastMessage>> reader = (MutableReader<DeserializationDelegate<MulticastMessage>>) inputReader;
-            @SuppressWarnings({ "unchecked", "rawtypes" })
-            final MutableObjectIterator<MulticastMessage> iter = new MulticastReaderIterator(reader, (TypeSerializer<MulticastMessage>) serializerFactory.getSerializer());
-            return iter;
+			//NOTE: multicast refactor:
+			@SuppressWarnings("unchecked")
+			MutableReader<DeserializationDelegate<MulticastMessage>> reader = (MutableReader<DeserializationDelegate<MulticastMessage>>) inputReader;
+			@SuppressWarnings({ "unchecked", "rawtypes" })
+			final MutableObjectIterator<MulticastMessage> iter = new MulticastReaderIterator(reader, (TypeSerializer<MulticastMessage>) serializerFactory.getSerializer());
+			return iter;
 		} else {
 			// generic data type serialization
 			@SuppressWarnings("unchecked")
